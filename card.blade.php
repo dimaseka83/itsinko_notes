ffb9687082ae04f585c6ee47db004e1c48f31d92
Dhimas Eka Prasetya <49355589+dimaseka83@users.noreply.github.com>
2021-11-11 07:51:32 +0700

    Create card.blade.php

---
+++ card.blade.php
@@ -0,0 +1,58 @@
+                <div class="row">
+                    <div class="col">
+                        <div id="transferoneday" class="card active otg" style="background-color: #FEF7EA">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk dengan jumlah stok 20 sampai 10</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div id="transferoneday" class="card otg" style="background-color: #FFBD67">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk dengan jumlah stok 9 sampai 5</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div id="transferoneday" class="card otg" style="background-color: #FF6464">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk dengan jumlah stok 4 sampai 1</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div id="transferoneday" class="card otg" style="background-color: #FEF7EA">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk masuk 6 bulan sampai 3 bulan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div id="transferoneday" class="card otg" style="background-color: #FFBD67">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk masuk 1 tahun sampai 6 bulan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div id="transferoneday" class="card otg" style="background-color: #FA8282">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk masuk 1 tahun sampai 3 tahun</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div id="transferoneday" class="card otg" style="background-color: #FF6464">
+                            <div class="card-body text-center">
+                                <h4>10</h4>
+                                <p class="card-text">Produk masuk lebih dari 3 tahun</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>

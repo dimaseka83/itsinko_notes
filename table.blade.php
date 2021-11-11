b58f89aa222a787d85b0be79762eb5fd682844b4
Dhimas Eka Prasetya <49355589+dimaseka83@users.noreply.github.com>
2021-11-11 07:50:35 +0700

    Create table.blade.php

---
+++ table.blade.php
@@ -0,0 +1,41 @@
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Berdasarkan Jumlah Produk</h3>
+                                <div class="card-tools">
+                                    <!-- button with a dropdown -->
+                                    <div class="btn-group">
+                                      <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
+                                        <i class="fas fa-ellipsis-h"></i>
+                                      </button>
+                                      <div class="dropdown-menu" role="menu">
+                                        <a href="#" class="dropdown-item">Layout 1</a>
+                                        <a href="#" class="dropdown-item">Layout 2</a>
+                                        <a href="#" class="dropdown-item">Layout 2</a>
+                                      </div>
+                                    </div>
+                                  </div>
+                            </div>
+                            <div class="card-body">
+                                <table class="table jml-produk">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Produk</th>
+                                            <th>Jumlah</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td scope="row">1</td>
+                                            <td>Produk 1</td>
+                                            <td>10 Unit</td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="row">2</td>
+                                            <td>Produk 2</td>
+                                            <td>20 Unit</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
